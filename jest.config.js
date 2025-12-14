3ccc8b5bf81c85ffb951518a13a3dd2e05c55552
Nizhar Maulana <maulananizhar27@gmail.com>
2025-12-14 09:56:45 +0700

    fix(jest): exclude dist from coverage

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/server.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/", "dist/"],
   transform: {
     ...tsJestTransformCfg,
   },
